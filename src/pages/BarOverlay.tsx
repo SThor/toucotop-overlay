8a31eb2af9f04d99d76019b2dda0910d19f7f672
Silma Thoron <paulgivel@gmail.com>
2026-04-01 23:06:15 +0200

    fix: stop stream duration counter when stream ends, align label with ClockOverlay

--- src/pages/BarOverlay.tsx
+++ src/pages/BarOverlay.tsx
@@ -59,7 +59,7 @@ const BarOverlayContent = () => {
   };
 
   const formatStreamDuration = () => {
-    if (!twitch.streamInfo?.startedAt) {
+    if (!twitch.streamInfo?.isLive || !twitch.streamInfo?.startedAt) {
       return '00:00:00';
     }
     
@@ -111,7 +111,7 @@ const BarOverlayContent = () => {
 
       {/* Stream Duration Section */}
       <div className="bar-section bar-time-section">
-        <div className="bar-time-label">Stream Time</div>
+        <div className="bar-time-label">Stream Duration</div>
         <div className="bar-time-value">{formatStreamDuration()}</div>
       </div>
 
