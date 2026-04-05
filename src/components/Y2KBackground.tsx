0a85807146bbff0f0e8619db20912d66e6279f93
Silma Thoron <paulgivel@gmail.com>
2026-04-05 16:01:40 +0200

    refactor(y2k): remove unused LiquidMetal import from Y2KBackground component

--- src/components/Y2KBackground.tsx
+++ src/components/Y2KBackground.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { LiquidMetal } from '@paper-design/shaders-react';
 import '../styles/Y2KTheme.css';
 import '@fontsource/unifrakturmaguntia/400.css';
 import '@fontsource-variable/climate-crisis/index.css';
